25539c5d40f38bfea82f2b37737e1f83d2942ec8
pelayocurso <pelayocurso@gmail.com>
2017-07-13 17:28:02 +0200

    Con moq

--- ejercicio11Test/TermomixTest.cs
+++ ejercicio11Test/TermomixTest.cs
@@ -1,81 +1,55 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ejercicio11;
+using Moq;
 
 namespace ejercicio11Test
 {
     [TestClass]
     public class TermomixTest
     {
-
-        [TestMethod]
-        public void TestPesar()
-        {
-            Termomix sut = Termomix.Instance;
-            Alimento ali = new Alimento("chope", 1, true);
-
-            Assert.AreEqual(1, sut.Pesar(ali));
-        }
-
-        public void TestCocinar()
-        {
-            Termomix sut = Termomix.Instance;
-            Alimento ali_uno = new Alimento("chope", 1, false);
-            Alimento ali_dos = new Alimento("chope", 1, false);
-            Plato plato = sut.Cocinar(ali_uno, ali_dos);
-
-            Assert.IsTrue(ali_uno.cocinado);
-
-            Alimento ali_tres = new Alimento("chope", 1, true);
-            Alimento ali_cuatro = new Alimento("chope", 1, true);
-            Plato test = new Plato(ali_tres, ali_cuatro);
-
-            Assert.IsTrue(
-                plato.Equals(test)
-            );
-        }
-
-        public void TestValidar()
-        {
-            Termomix sut = Termomix.Instance;
-            Alimento ali_uno = new Alimento("chope", 1, false);
-            Alimento ali_dos = new Alimento("chope", 1, false);
-
-            Assert.IsTrue(sut.Validar(ali_uno, ali_dos));
-        }
-
         [TestMethod]
         public void TestPesarYCocinar()
         {
+
+            var mockPesoService = new Mock<IPeso>();
+            var mockCocinaService = new Mock<ICocina>();
+
+            mockPesoService.Setup(peso => peso.Pesar(It.IsAny<Alimento>()))
+                .Returns((Alimento p) => p.peso);
+            mockPesoService.Setup(peso => peso.Pesar(It.IsAny<Alimento>()))
+                .Returns((Alimento p) => p.peso);
+
+            mockCocinaService.Setup(cocina => cocina.Cocinar(It.IsAny<Alimento>(), It.IsAny<Alimento>()))
+                .Callback((Alimento a1, Alimento a2) =>
+                {
+                    a1.cocinado = true;
+                    a2.cocinado = true;
+                });
+
+            IPeso pesoService = mockPesoService.Object;
+            ICocina cocinaService = mockCocinaService.Object;
+
             Termomix sut = Termomix.Instance;
+
+            sut.peso = pesoService;
+            sut.cocina = cocinaService;
+
             Alimento ali_uno = new Alimento();
             Alimento ali_dos = new Alimento();
 
             Plato plato_uno = sut.PesarYCocinar(ali_uno, ali_dos);
+            // Plato plato_uno = sut.Cocinar(ali_uno, ali_dos); // Ejemplo de error en el que no hace dos Pesar()
 
             Alimento ali_tres = new Alimento("chope", 0, true);
             Alimento ali_cuatro = new Alimento("chope", 0, true);
             Plato plato_dos = new Plato(ali_tres, ali_cuatro);
 
+            mockPesoService.Verify(peso => peso.Pesar(It.IsAny<Alimento>()), Times.AtLeast(2));
+            mockCocinaService.Verify(cocina => cocina.Cocinar(It.IsAny<Alimento>(), It.IsAny<Alimento>()), Times.AtLeastOnce);
+
             Assert.IsTrue(
                 plato_uno.Equals(plato_dos)
-            );
-            /*Assert.IsFalse(
-                new Plato(null, new Alimento(0, true)).Equals(plato)
-            );*/
-        }
-
-        [TestMethod]
-        public void TestValidarYCocinar()
-        {
-            Termomix sut = Termomix.Instance;
-            Alimento ali_uno = new Alimento("chope", 1, false);
-            Alimento ali_dos = new Alimento("chope", 1, false);
-
-            Plato plato = sut.ValidarYCocinar(ali_uno, ali_dos);
-
-            Assert.IsTrue(
-                plato.Equals(new Plato(new Alimento("chope", 1, true), new Alimento("chope", 1, true)))
             );
             /*Assert.IsFalse(
                 new Plato(null, new Alimento(0, true)).Equals(plato)
---
+++ ejercicio11Test/TermomixTestIntegracion.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ejercicio11;
+
+namespace ejercicio11Test
+{
+    [TestClass]
+    public class TermomixTestIntegracion
+    {
+
+        [TestMethod]
+        public void TestPesar()
+        {
+            Termomix sut = Termomix.Instance;
+            Alimento ali = new Alimento("chope", 1, true);
+
+            Assert.AreEqual(1, sut.Pesar(ali));
+        }
+
+        public void TestCocinar()
+        {
+            Termomix sut = Termomix.Instance;
+            Alimento ali_uno = new Alimento("chope", 1, false);
+            Alimento ali_dos = new Alimento("chope", 1, false);
+            Plato plato = sut.Cocinar(ali_uno, ali_dos);
+
+            Assert.IsTrue(ali_uno.cocinado);
+
+            Alimento ali_tres = new Alimento("chope", 1, true);
+            Alimento ali_cuatro = new Alimento("chope", 1, true);
+            Plato test = new Plato(ali_tres, ali_cuatro);
+
+            Assert.IsTrue(
+                plato.Equals(test)
+            );
+        }
+
+        public void TestValidar()
+        {
+            Termomix sut = Termomix.Instance;
+            Alimento ali_uno = new Alimento("chope", 1, false);
+            Alimento ali_dos = new Alimento("chope", 1, false);
+
+            Assert.IsTrue(sut.Validar(ali_uno, ali_dos));
+        }
+
+        [TestMethod]
+        public void TestPesarYCocinar()
+        {
+            Termomix sut = Termomix.Instance;
+            Alimento ali_uno = new Alimento();
+            Alimento ali_dos = new Alimento();
+
+            Plato plato_uno = sut.PesarYCocinar(ali_uno, ali_dos);
+
+            Alimento ali_tres = new Alimento("chope", 0, true);
+            Alimento ali_cuatro = new Alimento("chope", 0, true);
+            Plato plato_dos = new Plato(ali_tres, ali_cuatro);
+
+            Assert.IsTrue(
+                plato_uno.Equals(plato_dos)
+            );
+            /*Assert.IsFalse(
+                new Plato(null, new Alimento(0, true)).Equals(plato)
+            );*/
+        }
+
+        [TestMethod]
+        public void TestValidarYCocinar()
+        {
+            Termomix sut = Termomix.Instance;
+            Alimento ali_uno = new Alimento("chope", 1, false);
+            Alimento ali_dos = new Alimento("chope", 1, false);
+
+            Plato plato = sut.ValidarYCocinar(ali_uno, ali_dos);
+
+            Assert.IsTrue(
+                plato.Equals(new Plato(new Alimento("chope", 1, true), new Alimento("chope", 1, true)))
+            );
+            /*Assert.IsFalse(
+                new Plato(null, new Alimento(0, true)).Equals(plato)
+            );*/
+        }
+    }
+}
